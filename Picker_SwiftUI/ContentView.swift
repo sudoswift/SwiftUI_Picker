11b06fe0f84886fdbb3dab1af60eb31f4de14c62
sudoswift <sudoswift@gmail.com>
2021-05-28 01:49:03 +0900

    Initial Commit

---
+++ Picker_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Picker_SwiftUI
+//
+//  Created by izvern on 2021/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Picker_SwiftUI/Picker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picker_SwiftUIApp.swift
+//  Picker_SwiftUI
+//
+//  Created by izvern on 2021/05/28.
+//
+
+import SwiftUI
+
+@main
+struct Picker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
