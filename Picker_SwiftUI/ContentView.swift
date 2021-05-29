7ebb05509f05fb9ed628c89093e3536f5695d975
sudoswift <sudoswift@gmail.com>
2021-05-29 15:28:01 +0900

    add filter

--- Picker_SwiftUI/ContentView.swift
+++ Picker_SwiftUI/ContentView.swift
@@ -7,61 +7,77 @@
 
 import SwiftUI
 
+enum School: String, CaseIterable {
+    case elementary = "초등학교"
+    case middle = "중학교"
+    case high = "고등학교"
+}
+
+struct MyFried: Identifiable, Hashable {
+    var id = UUID()
+    var name: String
+    var school: School
+}
+
 struct ContentView: View {
     
-    @State private var selectionValue = 0
+    @State private var filteredValue = School.elementary
     
-    let myColorArray = ["레드","그린","블루"]
-    
-    //Swift에서 func 사용법
-    //func 함수명(파라미터: 파라미터의 타입(Int,String,Bool ...)) -> 반환할 타입{}
-    func changeColor(index: Int) -> Color{
-        //switch 문을 사용해 index가 0일 경우 red... 으로 설정해주고 default는 red로 설정해준다.
-        switch index {
-        case 0:
-            return Color.red
-        case 1:
-            return Color.green
-        case 2:
-            return Color.blue
-        default:
-            return Color.red
+    @State private var myFriendsList = [MyFried]()
+    //생성자(init) 메서드
+    init() {
+        var newList = [MyFried]()
+        for i in 0...20 {
+            let newFriend = MyFried(name: "내 친구 \(i)", school: School.allCases.randomElement()!)
+            newList.append(newFriend)
         }
-        
+        _myFriendsList = State(initialValue: newList)
     }
     
     var body: some View {
-        
-        VStack(alignment: .center){
-            
-            Circle()
-                .frame(width: 60, height: 60)
-                .foregroundColor(self.changeColor(index: selectionValue))
-            
-            Text("세그먼트 value : \(selectionValue)")
-            Text("선택된 색 : \(myColorArray[selectionValue])")
-            // 같은 뷰에서 picker는 같은 Binding을 공유한다.
-            Picker("", selection: $selectionValue, content: {
-                Text("레드").tag(0)
-                Text("그린").tag(1)
-                Text("블루").tag(2)
+        VStack{
+            Text("선택된 필터 : \(filteredValue.rawValue)")
+            Picker("", selection: $filteredValue, content: {
+                Text("초등학교").tag(School.elementary)
+                Text("중학교").tag(School.middle)
+                Text("고등학교").tag(School.high)
             })
             .padding(.horizontal, 50.0)
-            .pickerStyle(SegmentedPickerStyle()) // picker style 이 segmentedpickerstyle로 변경됨
+            .pickerStyle(SegmentedPickerStyle())
             
-            Picker("", selection: $selectionValue, content: {
-                Text("레드").tag(0)
-                Text("그린").tag(1)
-                Text("블루").tag(2)
-            })
-            .padding(.horizontal, 50.0)
-            .frame(width:50, height: 100)
-            .clipped()
-            .border(changeColor(index: selectionValue), width:10)
-
-        } // VStack
-        .padding()
+            List{
+                ForEach(myFriendsList.filter{
+                    $0.school == filteredValue
+                }){ friendItem in
+                GeometryReader{ GeometryProxy in
+                    HStack {
+                        Text("name: \(friendItem.name)")
+                            .frame(width: GeometryProxy.size.width / 3)
+                        Divider()
+                        Text("school: \(friendItem.school.rawValue)")
+                            .frame(width: GeometryProxy.size.width / 2)
+                    }
+                } // Geometry
+                }
+            }
+            
+//            List{
+//                ForEach(myFriendsList, id: \.self){ friendItem in
+//                    GeometryReader{ GeometryProxy in
+//                        HStack {
+//                            Text("name: \(friendItem.name)")
+//                                .frame(width: GeometryProxy.size.width / 3)
+//                            Divider()
+//                            Text("school: \(friendItem.school.rawValue)")
+//                                .frame(width: GeometryProxy.size.width / 2)
+//                        }
+//                    } // Geometry
+//                } // ForEach
+//            } //List
+            
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
